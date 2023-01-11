7d91445d7d7d313288da2ddaccf0ca975c704b7e
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2023-01-11 19:53:38 +0300

    deta deploy

---
+++ url_shortener/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const chalk = require("chalk");
+const config = require("config");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json({ extended: true }));
+app.use(cors());
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/link", require("./routes/link.routes"));
+
+const staticPath = path.resolve(__dirname, "client");
+
+app.use("/", express.static(staticPath));
+
+const indexPath = path.resolve(staticPath, "index.html");
+
+app.get("*", (req, res) => {
+  res.sendFile(indexPath);
+});
+
+const PORT = config.get("port") || 8080;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"));
+    console.log(chalk.cyanBright("MongoDB connected"));
+  } catch (error) {
+    console.log(chalk.red(`Server error ${error}`));
+    process.exit();
+  }
+}
+
+start();
+
+module.exports = app;
